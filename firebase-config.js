3b252135e935dcd9cbcb1f7bc07d2b90b21f846f
Azeem202920 <azeem.maisaloon@gmail.com>
2026-03-11 22:01:47 +0000

    Deploying to gh-pages from @ Azeem202920/complaint-manager-web@6af4baa33486d5e01019992e20c18ba6ceced72f 🚀

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// web/firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyC2qkp6xSzD21A7y5jnEOiv8X4LRrK4cnY",
+  authDomain: "complaint-manager-app-16d78.firebaseapp.com",
+  projectId: "complaint-manager-app-16d78",
+  storageBucket: "complaint-manager-app-16d78.firebasestorage.app",
+  messagingSenderId: "261364073312",
+  appId: "1:261364073312:web:b7828fbfac14fc416f1405",
+  measurementId: "G-CHKEBHS56C"
+};
